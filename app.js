aa5f9772c1433e53b83fb3ae93627c8ea52caeab
Cyprian Adamski <cyprian.adamski@gmail.com>
2023-04-25 15:24:21 +0200

    node first project

---
+++ app.js
@@ -0,0 +1,40 @@
+const os = require('os');
+// console.log('Platform: ', os.platform());
+// console.log('Arch: ', os.arch());
+// console.log('userInfoo', os.userInfo());
+
+const fs = require('fs');
+
+const genders = ['f', 'm'];
+const maleNames = ['John', 'Mark', 'Bill'];
+const femaleNames = ['Iga', 'Ola', 'Ala'];
+const lastNames = ['Zając', 'Kowal', 'Tusk'];
+
+const randChoice = (arr) => {
+  const index = Math.floor(Math.random() * arr.length);
+  return arr[index];
+};
+
+const people = [];
+
+for (let i = 0; i < 20; i++) {
+  const obj = {};
+  const gender = randChoice(genders);
+  const firstName =
+    gender === 'm' ? randChoice(maleNames) : randChoice(femaleNames);
+  const lastName = randChoice(lastNames);
+  const age = Math.floor(Math.random() * (78 - 18 + 1) + 18);
+
+  obj.gender = gender;
+  obj.firstName = firstName;
+  obj.lastName = lastName;
+  obj.age = age;
+
+  people.push(obj);
+}
+console.log(people);
+const data = JSON.stringify(people);
+fs.writeFile('people.json', data, (err) => {
+  if (err) throw err;
+  console.log('The file has been saved!');
+});
